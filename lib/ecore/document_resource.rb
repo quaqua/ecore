ad8dac5d0ea38d97dfdfa87dca74ba3b65de8431
thorsten zerha <quaqua@tastenwerk.com>
2011-11-20 19:23:51 +0100

    bugfix again mysql

--- lib/ecore/document_resource.rb
+++ lib/ecore/document_resource.rb
@@ -84,6 +84,7 @@ module Ecore
             index   :label_ids
 
             attrs.each_pair do |name, arr|
+              column name, :text, arr[1] if arr[0] == :text
               String name, arr[1] if arr[0] == :string || arr[0] == String
               Fixnum name, arr[1] if arr[0] == :integer || arr[0] == Fixnum
               DateTime name, arr[1] if arr[0] == :datetime || arr[0] == DateTime
